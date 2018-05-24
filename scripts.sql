e3cc83725189208c3b2d36eabd79d5b9566fbaf7
acosson <33237516+wololobzh@users.noreply.github.com>
2018-05-24 14:48:18 +0200

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,2 @@
---Création de la bdd bddmemory
 CREATE DATABASE bddmemory;
---Création de la table Times avec une clé primaire auto incrémentée.
 CREATE TABLE `bddmemory`.`Times` ( `id` INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT , `time` INT NOT NULL);
