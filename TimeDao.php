31f8429eca1850c524501a2d62731c3b1f52649b
acosson <33237516+wololobzh@users.noreply.github.com>
2018-05-24 09:26:51 +0200

    Update TimeDao.php

--- TimeDao.php
+++ TimeDao.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* SCRIPTS A EXECUTER SUR LE SERVEUR DE BASE DE DONNEES (via phpMyAdmin).
+* SCRIPT A EXECUTER SUR LE SERVEUR DE BASE DE DONNEES (via phpMyAdmin).
 *
 * CREATE DATABASE bddmemory;
 *
@@ -16,7 +16,7 @@ const LOGIN = 'root';
 const MOT_DE_PASSE = '';
 
 
-//Si TimeDao est appelé avec le paramètre 'time' alors on est dans le cas ou l'on désire enregistrer un temps.
+//Si TimeDao.php est appelé avec le paramètre 'time' alors on est dans le cas ou l'on désire enregistrer un temps.
 if (isset($_POST['time']))
 {
 	insertOneTime($_POST['time']);
@@ -24,16 +24,16 @@ if (isset($_POST['time']))
 //Sinon on est dans le cas ou l'on désire récupérer les meilleurs temps.
 else
 {
-	//Récupération d'un tableau avec les resultats
+	//Récupération d'un tableau avec les résultats
 	$meilleursTemps = getTheThreeBestTime();
 	
 	echo("Meilleurs scores <br/>");
 	
-	//Affichage de chaque résulat.
+	//Affichage de chaque résultat.
 	foreach ($meilleursTemps as $value)
 	{
 		echo($value . " ms <br/>");
-    }
+    	}
 }
 
 /**
@@ -49,59 +49,59 @@ function getConnection()
 */
 function getTheThreeBestTime()
 {
-	#Code à risque étant donné que l'on se connecte à une base de données
-	#La base de données est un élément exterieur que l'on ne maitrise pas.
+    #Code à risque étant donné que l'on se connecte à une base de données
+    #La base de données est un élément extérieur que l'on ne maîtrise pas.
     try
     {
 		#Récupération d'une variable représentant une connexion à la bdd.
 		$cnx = getConnection();
 		
-		#Création d'une requete contenant un paramètre
+		#Création d'une requête contenant un paramètre (?).
 		$requete = $cnx->prepare('SELECT time FROM Times ORDER BY time ASC LIMIT ?');
 
-		#On remplace le paramètre 1 par la valeur 3
+		#On remplace le paramètre 1 par la valeur 3.
 		$requete->bindValue(1, 3, PDO::PARAM_INT);
 
-		#On execute la requête.
+		#On exécute la requête.
 		$requete->execute();
 
-		#Recupération du resultat.
+		#Récupération du résultat.
 		$result = $requete->fetchAll(\PDO::FETCH_COLUMN);
 
-		#On retourne le resultat.
+		#On retourne le résultat.
 		return $result;
   }
   #Si il y a une erreur
   catch (PDOException $e)
   {
-		#On affiche un message d'erreur
+		#On affiche un message d'erreur.
 		echo("Impossible d'afficher les résultats <br/>");
 		#On log l'info afin de mieux comprendre le problème et pour la maintenance.
 		error_log ( "Erreur dans getTheThreeBestTime : " . $e );
-		#On quitte le script
+		#On quitte le script.
 		die();
   }
 }
 /**
-* Permet d'inserer un temps dans la table.
+* Permet d'insérer un temps dans la table.
 */
 function insertOneTime($time)
 {
 	#Code à risque étant donné que l'on se connecte à une base de données
-	#La base de données est un élément exterieur que l'on ne maitrise pas.
+	#La base de données est un élément exterieur que l'on ne maîtrise pas.
 	try
 	{
 		#Récupération d'une variable représentant une connexion à la bdd.
 		$cnx = getConnection();
 
-		#Création d'une requete contenant un paramètre
+		#Création d'une requête contenant un paramètre (?).
 		#On met la valeur null pour la colonne id car elle est auto incrementée.
 		$handle = $cnx->prepare("INSERT INTO times (`id`, `time`) VALUES (NULL, ?)");
 
-		#On remplace le paramètre 1 par la valeur du temps passé en paramètre de la fonction
+		#On remplace le paramètre 1 par la valeur du temps passé en paramètre de la fonction.
 		$handle->bindValue(1, $time, PDO::PARAM_INT);
 
-		#On execute la requete
+		#On exécute la requete.
 		$handle->execute();
 	}
 	#Si il y a une erreur
